c56f65df9cfb20f68588d2ac20ac3836cf95f31a
SebastianZ25 <zambranosebastian032@gmail.com>
2025-06-06 21:39:50 -0500

    selecciones agregadas todo ok

---
+++ Selecciones/Tabla 5 videojuegos.sql
@@ -0,0 +1,29 @@
+create table videojuegos (
+codigo int not null,
+nombre varchar (100) not null,
+descripcion varchar (300),
+valoracion int not null,
+constraint videojuegos_pk primary key (codigo)
+)
+
+insert into videojuegos(codigo,nombre,descripcion,valoracion)
+values (111,'Cyberpunk 2077','RPG futurista,mundo abierto', 4)
+
+insert into videojuegos(codigo,nombre,descripcion,valoracion)
+values (222,'God of War Ragnarok','Aventura,accion,narrativa potente', 5)
+
+insert into videojuegos(codigo,nombre,descripcion,valoracion)
+values (333,'Red Dead Redemption 2','Mundo abierto inmersivo', 5)
+
+insert into videojuegos(codigo,nombre,descripcion,valoracion)
+values (444,'Elden Ring','Accion,mundo abierto', 3)
+
+insert into videojuegos(codigo,nombre,descripcion,valoracion)
+values (555,'Resident Evil 4','Survival,horror', 4)
+
+--1
+select * from videojuegos where nombre like 'C%'
+--2
+select * from videojuegos where valoracion between 9 and 10
+--3
+select * from videojuegos where descripcion is null
---
+++ Selecciones/Tabla4 registros_entrada.sql
@@ -0,0 +1,45 @@
+create table registros_entrada (
+codigo_registro int not null,
+cedula_empleado char (10),
+fecha date not null,
+hora time not null,
+constraint registros_pk primary key (codigo_registro)
+)
+
+
+insert into registros_entrada (codigo_registro,cedula_empleado,fecha,hora)
+values (1111,'1198641011','2025/06/03','08:00')
+
+insert into registros_entrada (codigo_registro,cedula_empleado,fecha,hora)
+values (2222,'1198641012','2025/06/03','08:15')
+
+insert into registros_entrada (codigo_registro,cedula_empleado,fecha,hora)
+values (3333,'1198641013','2025/06/03','08:30')
+
+insert into registros_entrada (codigo_registro,cedula_empleado,fecha,hora)
+values (4444,'1198641014','2025/06/04','09:00')
+
+insert into registros_entrada (codigo_registro,cedula_empleado,fecha,hora)
+values (5555,'1198641015','2025/06/04','09:10')
+
+insert into registros_entrada (codigo_registro,cedula_empleado,fecha,hora)
+values (6666,'1198641016','2025/06/04','09:35')
+
+insert into registros_entrada (codigo_registro,cedula_empleado,fecha,hora)
+values (7777,'1198641017','2025/06/05','07:45')
+
+insert into registros_entrada (codigo_registro,cedula_empleado,fecha,hora)
+values (8888,'1198641018','2025/06/05','08:05')
+
+insert into registros_entrada (codigo_registro,cedula_empleado,fecha,hora)
+values (9999,'1198641099','2025/06/05','08:20')
+
+insert into registros_entrada (codigo_registro,cedula_empleado,fecha,hora)
+values (9595,'1198641010','2025/06/05','08:55')
+
+--1
+select cedula_empleado,fecha,hora  from registros_entrada
+--2
+select * from registros_entrada where hora between '7:00' and '14:00'
+--3
+select * from registros_entrada where hora >'8:00'
